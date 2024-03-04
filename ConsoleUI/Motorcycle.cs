b1879d004cc0893106f32ef296a5e6e115869f89
bdurushia <b.durushia@gmail.com>
2024-03-04 12:05:26 -0600

    Completed exercise.

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    /* 
+    * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
+    * Add a distinct property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+    * Provide the implementations for the abstract methods
+    * Only in the Motorcycle class will you override the virtual drive method
+    */
+    public class Motorcycle : Vehicle
+    {
+        public bool HasSideCar { get; set; }
+        public override void DriveAbstract()
+        {
+            Console.WriteLine($"{Name} Motorcycle drives on only TWO WHEELS.\n");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"{Name} Drives REALLY FAST.\n");
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,22 +17,8 @@ namespace ConsoleUI
 
             #region Vehicles
 
-            /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties: Year, Make, and Model
-             * Set the defaults of the properties to something generic in the Vehicle class
-             * Vehicle class shall have an abstract method called DriveAbstract with no implementation.
-             * Vehicle class shall have a virtual method called DriveVirtual with a base implementation.
-             */
-
-            /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distinct property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
-            */
-
             // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
@@ -40,12 +26,67 @@ namespace ConsoleUI
              * Set the properties values with object initializer syntax
              */
 
+            Car golfR = new Car()
+            {
+                Name = "Big Turbo Golf",
+                Year = "2017",
+                Make = "VW",
+                Model = "Golf R",
+                HasTrunk = true
+            };
+
+            Vehicle suzuGSX = new Motorcycle()
+            {
+                Name = "Suzuki Cafe Racer",
+                Year = "2001",
+                Make = "Suzuki",
+                Model = "GSX 1400",
+                HasSideCar = true
+            };
+
+            Vehicle audiRS5 = new Car()
+            {
+                Name = "Audi Turbo V8",
+                Year = "2014",
+                Make = "Audi",
+                Model = "RS5",
+                HasTrunk = true
+            };
+
+            Motorcycle yamR3 = new Motorcycle()
+            {
+                Name = "Yamaha Rocket",
+                Year = "2016",
+                Make = "Yamaha",
+                Model = "R3",
+                HasSideCar = false
+            };
+
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate through the list and display each of the properties
              */
 
+            vehicles.Add(audiRS5);
+            vehicles.Add(yamR3);
+            vehicles.Add(golfR);
+            vehicles.Add(suzuGSX);
+
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("YEAR\t\tMAKE\t\tMODEL");
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine($"{vehicle.Year}\t\t{vehicle.Make}\t\t{vehicle.Model}");
+            }
+
+            Console.WriteLine("------------------------------------------------------\n\n");
+
+
             // Call each of the drive methods for one car and one motorcycle
+            yamR3.DriveAbstract();
+            yamR3.DriveVirtual();
+            golfR.DriveAbstract();
+            golfR.DriveVirtual();
 
             #endregion            
             Console.ReadLine();
